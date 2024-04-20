c4145bbe997adc6a6e68bd7d3a7e228c588f0d60
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-04-20 01:25:56 +0000

    ...

--- descargarPdf.js
+++ descargarPdf.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const HttpProvider = Web3.providers.HttpProvider;
 const Certificate = require('./Contract_sol_Certificate.abi').abi;
 
-const provider = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+const provider = Web3.providers.HttpProvider('http://127.0.0.1:8545');
 const web3Instance = new Web3(provider);
 
 const contractAddress = '0xdecf98879b05e0fbf586a5013ad7913ee71cc156'; // Dirección del contrato inteligente
